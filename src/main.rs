fb64449519612c4b699ccff4ce01a223e010cd91
pavlovich_nn <pavlovich.work@gmail.com>
2023-06-04 22:26:28 +0300

    ДЗ №2 (занятие №5): Умный дом

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+struct _SmartSocket;
+
+impl _SmartSocket {
+    fn _on(&self) {
+        todo!();
+    }
+
+    fn _off(&self) {
+        todo!();
+    }
+
+    fn _get_power_consumption(&self) -> f32 {
+        todo!()
+    }
+}
+
+struct _Thermometer;
+
+impl _Thermometer {
+    fn _get_temperature(&self) -> f32 {
+        todo!()
+    }
+}
+
+fn main() {}
