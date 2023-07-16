c082ad62255f6945257d45fc37b80393a27c30d6
pavlovich_nn <pavlovich.work@gmail.com>
2023-07-16 21:22:01 +0300

    собирается, нет предупреждений

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,159 @@
-#[derive(Debug, Clone)]
-struct Device {
-    name: String,
+// Метка todo - реализовать самостоятельно
+
+// ***** Пример библиотеки "Умный дом" со статическим содержимым
+
+trait DeviceInfoProvider {
+    // todo: метод, возвращающий состояние устройства по имени комнаты и имени устройства
+    fn get_device_state(&self, room: &str, device: &str) -> String;
 }
 
+struct SmartHouse {
+    /* todo: данные умного дома */
+    rooms: [String; 2],
+}
 
-struct SmartSocket;
-
-impl SmartSocket {
-    fn on(&self) {
-        todo!();
+impl SmartHouse {
+    fn new() -> Self {
+        // todo!("реализовать инициализацию дома")
+        Self {
+            rooms: ["кухня".to_string(), "спальня".to_string()],
+        }
     }
 
-    fn off(&self) {
-        todo!();
+    fn get_rooms(&self) -> [String; 2] {
+        // Размер возвращаемого массива можно выбрать самостоятельно
+        // todo!("список комнат");
+        self.rooms.clone()
     }
 
-    fn get_power_consumption(&self) -> f32 {
-        todo!()
+    fn devices(&self, room: &str) -> [String; 3] {
+        // Размер возвращаемого массива можно выбрать самостоятельно
+        // todo!("список устройств в комнате `room`")
+        match room {
+            "кухня" => ["1".to_string(), "2".to_string(), "3".to_string()],
+            "спальня" => ["1".to_string(), "2".to_string(), "3".to_string()],
+            _ => panic!("Неизвестное название комнаты: {}", room),
+        }
+    }
+
+    fn create_report(
+        &self,
+        /* todo: принять обобщённый тип предоставляющий информацию об устройствах */
+        device_info_provider: &impl DeviceInfoProvider,
+    ) -> String {
+        // todo!("перебор комнат и устройств в них для составления отчёта")
+        let mut report = "".to_string();
+        for room in self.get_rooms().iter() {
+            for device in self.devices(room).iter() {
+                report.push_str(&device_info_provider.get_device_state(room, device));
+            }
+        }
+        report
     }
 }
 
-struct Thermometer;
+// ***** Пример использования библиотеки умный дом:
 
-impl Thermometer {
-    fn get_temperature(&self) -> f32 {
-        todo!()
+trait SmartDeviceStateInfo {
+    fn get_state_info(&self) -> String;
+}
+
+// Пользовательские устройства:
+struct SmartSocket {
+    on: bool,
+}
+struct SmartThermometer {
+    on: bool,
+    temperature: f32,
+}
+
+impl SmartDeviceStateInfo for SmartSocket {
+    fn get_state_info(&self) -> String {
+        if self.on {
+            "состояние: включен".to_string()
+        } else {
+            "состояние: выключен".to_string()
+        }
     }
 }
 
-
-struct Room {
-    name: String,
-    devices: Vec<Device>,
+impl SmartDeviceStateInfo for SmartThermometer {
+    fn get_state_info(&self) -> String {
+        if self.on {
+            format!("состояние: включен; температура: {}", self.temperature)
+        } else {
+            "состояние: выключен".to_string()
+        }
+    }
 }
 
-
-struct House {
-    name: String,
-    rooms: Vec<Room>,
+// Пользовательские поставщики информации об устройствах.
+// Могут как хранить устройства, так и заимствывать.
+struct OwningDeviceInfoProvider {
+    socket: SmartSocket,
+}
+struct BorrowingDeviceInfoProvider<'a, 'b> {
+    socket: &'a SmartSocket,
+    thermo: &'b SmartThermometer,
 }
 
+// todo: реализация трейта `DeviceInfoProvider` для поставщиков информации
+impl DeviceInfoProvider for OwningDeviceInfoProvider {
+    fn get_device_state(&self, room: &str, device: &str) -> String {
+        format!(
+            "Комната: {}; устройство: {}; {}\n",
+            room,
+            device,
+            self.socket.get_state_info()
+        )
+    }
+}
 
-impl House {
-    fn get_rooms(&self) -> Vec<String> {
-        self.rooms.iter().map(|room| room.name.clone()).collect()
-    }
-    
-    fn get_devices_in_room(&self, room_name: &str) -> Option<Vec<String>> {
-        self.rooms
-            .iter()
-            .find(|room| room.name == room_name)
-            .map(|room| room.devices.iter().map(|device| device.name.clone()).collect())
-    }
-    
-    fn get_device_state<T>(&self, room_name: &str, device_name: &str) -> Result<T, String>
-    where
-        T: ToString,
-    {
-        self.rooms
-            .iter()
-            .find(|room| room.name == room_name)
-            .and_then(|room| {
-                room.devices
-                    .iter()
-                    .find(|device| device.name == device_name)
-                    .map(|device| device.name.to_string())
-            })
-            .ok_or_else(|| format!("Device '{}' not found in room '{}'", device_name, room_name))
-            .map(|device_name| T::to_string(&device_name) as T)
+impl<'a, 'b> DeviceInfoProvider for BorrowingDeviceInfoProvider<'a, 'b> {
+    fn get_device_state(&self, room: &str, device: &str) -> String {
+        let mut report = "".to_string();
+        report.push_str(&format!(
+            "Комната: {}; устройство: {}; {}\n",
+            room,
+            device,
+            self.socket.get_state_info()
+        ));
+        report.push_str(&format!(
+            "Комната: {}; устройство: {}; {}\n",
+            room,
+            device,
+            self.thermo.get_state_info()
+        ));
+        report
     }
 }
 
 fn main() {
-    let mut devices = vec![
-        Device { name: "Light".to_string() },
-        Device { name: "Fan".to_string() },
-    ];
-    
-    let mut rooms = vec![
-        Room { name: "Living Room".to_string(), devices: devices.clone() },
-        Room { name: "Bedroom".to_string(), devices: devices.clone() },
-    ];
-    
-    let house = House { name: "My House".to_string(), rooms };
-    
-    let rooms = house.get_rooms();
-    println!("Rooms: {:?}", rooms);
+    // Инициализация устройств
+    let socket1 = SmartSocket { on: true };
+    let socket2 = SmartSocket { on: false };
+    let thermo = SmartThermometer {
+        on: true,
+        temperature: 36.6,
+    };
 
-    let devices = house.get_devices_in_room("Living Room");
-    println!("Devices in Living Room: {:?}", devices);
+    // Инициализация дома
+    let house = SmartHouse::new();
 
-    let device_state: Result<String, _> = house.get_device_state("Living Room", "Light");
+    // Строим отчёт с использованием `OwningDeviceInfoProvider`.
+    let info_provider_1 = OwningDeviceInfoProvider { socket: socket1 };
+    // todo: после добавления обобщённого аргумента в метод, расскоментировать передачу параметра
+    let report1 = house.create_report(&info_provider_1);
 
-    match device_state {
-        Ok(state) => println!("Light state in Living Room: {}", state),
-        Err(error) => println!("Error: {}", error),
-    }
+    // Строим отчёт с использованием `BorrowingDeviceInfoProvider`.
+    let info_provider_2 = BorrowingDeviceInfoProvider {
+        socket: &socket2,
+        thermo: &thermo,
+    };
+    // todo: после добавления обобщённого аргумента в метод, расскоментировать передачу параметра
+    let report2 = house.create_report(&info_provider_2);
+
+    // Выводим отчёты на экран:
+    println!("Report #1: {report1}");
+    println!("Report #2: {report2}");
 }
