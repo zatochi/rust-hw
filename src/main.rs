6a188676688fcb7a27c3b585864260a4ab6844ca
pavlovich_nn <pavlovich.work@gmail.com>
2023-07-16 19:16:46 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,71 @@
-struct _SmartSocket;
+#[derive(Debug, Clone)]
+struct Device {
+    name: String,
+}
 
-impl _SmartSocket {
-    fn _on(&self) {
-        todo!();
+struct Room {
+    name: String,
+    devices: Vec<Device>,
+}
+
+struct House {
+    name: String,
+    rooms: Vec<Room>,
+}
+
+impl House {
+    fn get_rooms(&self) -> Vec<String> {
+        self.rooms.iter().map(|room| room.name.clone()).collect()
     }
-
-    fn _off(&self) {
-        todo!();
+    
+    fn get_devices_in_room(&self, room_name: &str) -> Option<Vec<String>> {
+        self.rooms
+            .iter()
+            .find(|room| room.name == room_name)
+            .map(|room| room.devices.iter().map(|device| device.name.clone()).collect())
     }
-
-    fn _get_power_consumption(&self) -> f32 {
-        todo!()
+    
+    fn get_device_state<T>(&self, room_name: &str, device_name: &str) -> Result<T, String>
+    where
+        T: ToString,
+    {
+        self.rooms
+            .iter()
+            .find(|room| room.name == room_name)
+            .and_then(|room| {
+                room.devices
+                    .iter()
+                    .find(|device| device.name == device_name)
+                    .map(|device| device.name.to_string())
+            })
+            .ok_or_else(|| format!("Device '{}' not found in room '{}'", device_name, room_name))
+            .map(|device_name| T::to_string(&device_name) as T)
     }
 }
 
-struct _Thermometer;
+fn main() {
+    let mut devices = vec![
+        Device { name: "Light".to_string() },
+        Device { name: "Fan".to_string() },
+    ];
+    
+    let mut rooms = vec![
+        Room { name: "Living Room".to_string(), devices: devices.clone() },
+        Room { name: "Bedroom".to_string(), devices: devices.clone() },
+    ];
+    
+    let house = House { name: "My House".to_string(), rooms };
+    
+    let rooms = house.get_rooms();
+    println!("Rooms: {:?}", rooms);
 
-impl _Thermometer {
-    fn _get_temperature(&self) -> f32 {
-        todo!()
+    let devices = house.get_devices_in_room("Living Room");
+    println!("Devices in Living Room: {:?}", devices);
+
+    let device_state: Result<String, _> = house.get_device_state("Living Room", "Light");
+
+    match device_state {
+        Ok(state) => println!("Light state in Living Room: {}", state),
+        Err(error) => println!("Error: {}", error),
     }
 }
-
-fn main() {}
